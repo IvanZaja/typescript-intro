ef2dfddbc1f5636cab87aea5250b66c5e7cff3c3
IvanZaja <sayonbra@gmail.com>
2024-10-02 12:30:54 +0200

    Finished Typescript Section

---
+++ src/topics/09-generics.ts
@@ -0,0 +1,13 @@
+export function whatsMyType<T>( argument: T ): T {
+
+    return argument;
+}
+
+let amIString = whatsMyType<string>('Hola mundo');
+let amINumber = whatsMyType<number>(100);
+let amIArray = whatsMyType<number[]>([1,2,3,4,5])
+
+
+console.log(amIString)
+console.log(amINumber)
+console.log(amIArray)
